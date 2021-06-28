4fb07938b090dd271e886b5ce2360276065f0e72
shadowpog <79033248+shadowpog@users.noreply.github.com>
2021-06-28 16:55:53 +0600

    Add files via upload
    m.js is the main file. install.bat to install modules, run.bat to run the script.

---
+++ m.js
@@ -0,0 +1,148 @@
+const chalk = require("chalk");
+const prompt=require("prompt-sync")({sigint:true});
+const figlet = require('figlet')
+const a = console;
+const b = a.log;
+
+
+function hasx(string){
+return string.toLowerCase().includes("x")
+}
+
+function hasy(string){
+    return string.toLowerCase().includes("y")
+}
+
+function handle(option){
+
+    if(option === "1"){
+        a.clear()
+                figlet("Tic Tac Toe",(err,data)=>{
+                    b(chalk.blue(data))
+                })
+        setTimeout(()=>{
+            a.clear()
+            const x = "X"
+            const o = "O"
+            var table = `
+            \n|____|____|____|\n|____|____|____|\n|____|____|____|`
+            b(`
+            ${table}
+1       2      3
+4       5      6
+7       8      9 
+            `)
+            var places = [];
+            const bloccs = table.split("\n").join("").split("|")
+            for(var i in bloccs){
+                if(bloccs[i]==="____"){
+                    places.push(bloccs[i])
+                }
+            }
+            var match = false
+            while(!match){
+                var t = ["x","y"]
+                for(var i = 0;i<t.length;i++){
+                    b(`${t[i]}'s move.`)
+                    const choice = prompt().toLowerCase()
+                    const move = parseInt(choice) - 1
+                    
+                    if(places[move]){
+                        if(places[move].includes(t[0]) || places[move].includes(t[1])){
+                            b(chalk.red("That spot is taken."))
+                            continue
+                        }
+                        places[move] = `__${t[i]}_`
+                        var newc = `
+                        |_${places[0]}__|_${places[1]}__|_${places[2]}|
+                        |_${places[3]}__|_${places[4]}__|_${places[5]}|
+                        |_${places[6]}__|_${places[7]}__|_${places[8]}|
+                        1       2      3
+                        4       5      6
+                        7       8      9 
+                        `
+                        a.clear()
+                        b(newc)
+                        function checkifmatch(){
+                            if((hasx(places[0]) && hasx(places[1]) && hasx(places[2])))return true;
+                            if((hasx(places[3]) && hasx(places[4]) && hasx(places[5])))return true;
+                            if((hasx(places[6]) && hasx(places[7]) && hasx(places[8])))return true;
+                            if((hasx(places[0]) && hasx(places[3]) && hasx(places[6])))return true;
+                            if((hasx(places[1]) && hasx(places[4]) && hasx(places[7])))return true;
+                            if((hasx(places[2]) && hasx(places[5]) && hasx(places[8])))return true;
+                            if((hasx(places[2]) && hasx(places[4]) && hasx(places[6])))return true;
+                            if((hasx(places[0]) && hasx(places[4]) && hasx(places[8])))return true;
+
+
+                            if((hasy(places[0]) && hasy(places[1]) && hasy(places[2])))return true;
+                            if((hasy(places[3]) && hasy(places[4]) && hasy(places[5])))return true;
+                            if((hasy(places[6]) && hasy(places[7]) && hasy(places[8])))return true;
+                            if((hasy(places[0]) && hasy(places[3]) && hasy(places[6])))return true;
+                            if((hasy(places[1]) && hasy(places[4]) && hasy(places[7])))return true;
+                            if((hasy(places[2]) && hasy(places[5]) && hasy(places[8])))return true;
+                            if((hasy(places[2]) && hasy(places[4]) && hasy(places[6])))return true;
+                            if((hasy(places[0]) && hasy(places[4]) && hasy(places[8])))return true;
+
+                        }
+                        
+                        if(checkifmatch()){
+                            const winner = t[i]
+                            match = true
+                            figlet("GG",(err,data)=>{
+                                b(chalk.blue(data))
+                            })
+                            b(chalk.green(winner + " has won the game!"))
+                            b("Press any key to continue")
+                            prompt()
+                            process.exit()
+                        } else if(checkifmatch()){
+                            const winner = t[i]
+                            match = true
+                            figlet("GG",(err,data)=>{
+                                b(chalk.blue(data))
+                            })
+                            b(chalk.green(winner + " has won the game!"))
+                            b("Press any key to continue")
+                            prompt()
+                            process.exit()
+
+                        }
+                        
+                    } else {
+                        b(chalk.red("Invalid Choice."))
+                        continue
+                    }
+                }
+            }
+        },1000)
+    } else if(option === '2'){
+        b(chalk.green("Exiting..."))
+        setTimeout(()=>{process.exit()},1000)
+    } else if(option === "3"){
+        b(chalk.blue(`
+        ------------------------------------------------
+        `))
+        b(chalk.yellow(            `
+        Modules used: Chalk, Prompt-Sync, figlet
+        Made by LocalSimp#0001 aka _Shadow_#4579
+        `))
+        b(chalk.red(`
+        ---------- Press ENTER to continue -------------
+        `))
+        
+        prompt();
+
+    } else {
+        b(chalk.red("Invalid Choice") + "\n" + "Exiting...");
+        process.exit();
+    }
+}
+b(`
+Welcome to the weird gaem!1    
+[1] Play                   
+[2] Exit                   
+[3] Credits                
+`)
+b("What would you like to do?")
+const option = prompt()
+handle(option)
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+echo sup
+title Tic Tac Toe 2 Player
+cls
+node m.js
